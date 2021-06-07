94bba245b2bffe1f9610979ede81338752ca52f3
GMO <gusanum5@gmail.com>
2021-06-07 12:53:57 +0100

    Ready to push

--- lib/widgets/slide_item.dart
+++ lib/widgets/slide_item.dart
@@ -1,5 +1,6 @@
 import 'package:be_notified/model/slide.dart';
 import 'package:flutter/material.dart';
+
 class SlideItem extends StatelessWidget {
   final int index;
   SlideItem({this.index});
@@ -7,33 +8,39 @@ class SlideItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-    child:  Column(
+      child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
-          Image.asset(slideList[index].imageUrl,width: 250, height: 250,),
-          SizedBox(height: 40,),
+          Image.asset(
+            slideList[index].imageUrl,
+            width: 250,
+            height: 250,
+          ),
+          SizedBox(
+            height: 40,
+          ),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text(slideList[index].title,
-                style:TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w600
-                ),),
-
-              Text(slideList[index].tit ,
-                style:
-                TextStyle(
+              Text(
+                slideList[index].title,
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+              ),
+              Text(
+                slideList[index].tit,
+                style: TextStyle(
                     color: Color(0xFFDB3942),
                     fontSize: 20,
-                    fontWeight: FontWeight.w600
-                ),
+                    fontWeight: FontWeight.w600),
               ),
             ],
           ),
-          SizedBox(height: 15,),
-          Text(slideList[index].desc ,
+          SizedBox(
+            height: 15,
+          ),
+          Text(
+            slideList[index].desc,
             textAlign: TextAlign.center,
             style: TextStyle(
               fontSize: 16,
